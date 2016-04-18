50531af45d58f2f8eb1d2b17b9d57c7cea9dbda7
Latchezar Tzvetkoff <latchezar@tzvetkoff.net>
2016-04-18 16:38:35 +0300

    rewrite install.sh and make my.cnf regular file

--- install.sh
+++ install.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
 #
-# files to install
+# files to symlink @ ~/
 #
 
-H=(\
+HOME_SYMLINKS=(\
   .aprc           \
   .bash_profile   \
   .bashrc         \
   .bin            \
   .gemrc          \
   .irbrc          \
-  .my.cnf         \
   .nano           \
   .nanorc         \
   .pythonrc       \
@@ -23,36 +22,37 @@ H=(\
 # colors
 #
 
-R="\033[01;31m"
-G="\033[01;32m"
-Y="\033[01;33m"
-B="\033[01;34m"
-W="\033[00;00m"
+COLOR_RED="\033[01;31m"
+COLOR_GREEN="\033[01;32m"
+COLOR_YELLOW="\033[01;33m"
+COLOR_BLUE="\033[01;34m"
+COLOR_WHITE="\033[00;00m"
 
 #
 # this script's path
 #
 
-P=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
+ROOT=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
 
 #
-# force?
+# force flag
 #
-if [[ "${1}" = "-f" || "${1}" = "--force" ]]; then
-  F="yes"
+
+if [[ "${1}" = '-f' || "${1}" = '--force' ]]; then
+  FORCE='true'
 else
-  F=""
+  FORCE='false'
 fi
 
 #
-# question? [Y/n]
+# question? [y/N]
 #
 
-Q() {
-  echo -ne "${Y}${*} ${B}[Y/n]${W} "
+QUESTION() {
+  echo -ne "${COLOR_YELLOW}${*} ${COLOR_BLUE}[y/N]${COLOR_WHITE} "
   read -n 1 -r
   echo
-  if [[ ${REPLY} =~ ^[Yy]$ ]]; then
+  if [[ "${REPLY}" =~ ^[Yy]$ ]]; then
     return 0
   else
     return 1
@@ -60,33 +60,74 @@ Q() {
 }
 
 #
-# install files in ${HOME}
+# symlink files @ ~/
 #
 
-pushd ${HOME} >/dev/null
-for z in ${H[@]}; do
-  if [[ -e ./${z} ]]; then
-    if [[ -n ${F} ]] || Q "File ${W}${HOME}/${z}${Y} already exists. Overwrite?"; then
-      rm -rf -- ${z}
-    else
-      echo -e "${R}skip${W} ${z}"
-      continue
+create_home_symlinks() {
+  for symlink in "${HOME_SYMLINKS[@]}"; do
+    if [[ -e "${HOME}/${symlink}" ]]; then
+      if ${FORCE} || QUESTION "File ${COLOR_WHITE}${HOME}/${symlink}${COLOR_YELLOW} already exists. Overwrite?"; then
+        rm -rf -- "${HOME}/${symlink}"
+      else
+        echo -e "${COLOR_RED}skip${COLOR_WHITE} ${HOME}/${symlink}"
+        continue
+      fi
+    fi
+
+    echo -e "${COLOR_GREEN}install${COLOR_WHITE} ${HOME}/${symlink}"
+    ln -s "${ROOT}/${symlink}" "${HOME}/${symlink}"
+  done
+}
+
+#
+# create ~/.gitconfig
+#
+
+create_home_gitconfig() {
+  if [[ -e "${HOME}/.gitconfig" ]]; then
+    if ! ${FORCE} && ! QUESTION "File ${COLOR_WHITE}${HOME}/.gitconfig${COLOR_YELLOW} already exists. Overwrite?"; then
+      echo -e "${COLOR_RED}skip${COLOR_WHITE} ${HOME}/.gitconfig"
+      return 0
     fi
   fi
 
-  echo -e "${G}install${W} ${z}"
-  ln -s ${P}/${z} ./${z}
-done
-popd >/dev/null
+  git config --global user.name 'Latchezar Tzvetkoff'
+  git config --global user.email 'latchezar'$'\100''tzvetkoff'$'\056''net'
+  git config --global alias.st 'status'
+  git config --global alias.ci 'commit'
+  git config --global alias.co 'checkout'
+  git config --global alias.dt 'difftool'
+  git config --global alias.mt 'mergetool'
+  echo -e "${COLOR_GREEN}create${COLOR_WHITE} ${HOME}/.gitconfig"
+}
 
 #
-# configure git
+# create ~/.my.cnf
 #
 
-git config --global user.name 'Latchezar Tzvetkoff'
-git config --global user.email 'latchezar'$'\100''tzvetkoff'$'\056''net'
-git config --global alias.st 'status'
-git config --global alias.ci 'commit'
-git config --global alias.co 'checkout'
-git config --global alias.dt 'difftool'
-git config --global alias.mt 'mergetool'
+create_home_mycnf() {
+  if [[ -e "${HOME}/.my.cnf" ]]; then
+    if ! ${FORCE} && ! QUESTION "File ${COLOR_WHITE}${HOME}/.my.cnf${COLOR_YELLOW} already exists. Overwrite?"; then
+      echo -e "${COLOR_RED}skip${COLOR_WHITE} ${HOME}/.my.cnf"
+      return 0
+    fi
+  fi
+
+  cat >"${HOME}/.my.cnf" <<EOF
+[mysql]
+auto-rehash
+user=root
+
+[mysqldump]
+user=root
+EOF
+  echo -e "${COLOR_GREEN}create${COLOR_WHITE} ${HOME}/.my.cnf"
+}
+
+#
+# do the actual job
+#
+
+create_home_symlinks
+create_home_gitconfig
+create_home_mycnf
