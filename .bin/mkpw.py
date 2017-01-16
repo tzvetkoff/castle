e4718f743e0718725f9881f05c8b0df89c140874
Latchezar Tzvetkoff <latchezar@tzvetkoff.net>
2017-01-17 01:08:39 +0200

    argparse instead of optparse

--- .bin/mkpw.py
+++ .bin/mkpw.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python2
 
 from random import randint
-from optparse import OptionParser, SUPPRESS_HELP
+from argparse import ArgumentParser, SUPPRESS
 
 if __name__ == '__main__':
-	parser = OptionParser()
-	parser.add_option('-c', '--charset', dest='charset', type='string', default='0123456789abcdef', metavar='CHARSET', help='set charset [default: %default]')
-	parser.add_option('-l', '--length', dest='length', type='int', default=16, metavar='LENGTH', help='password length [default: %default]')
-	parser.add_option('-n', '--count', dest='count', type='int', default=8, metavar='COUNT', help='passwords to generate' '[default: %default]')
-	parser.add_option('-s', '--strong', action='store_true', dest='strong', default=False, help=SUPPRESS_HELP)
+	parser = ArgumentParser()
+	parser.add_argument('-c', '--charset', dest='charset', type=str, default='0123456789abcdef', metavar='CHARSET', help='set charset [default: %(default)s]')
+	parser.add_argument('-l', '--length', dest='length', type=int, default=16, metavar='LENGTH', help='password length [default: %(default)s]')
+	parser.add_argument('-n', '--count', dest='count', type=int, default=8, metavar='COUNT', help='passwords to generate [default: %(default)s]')
+	parser.add_argument('-s', '--strong', action='store_true', dest='strong', default=False, help=SUPPRESS)
 
-	options = parser.parse_args()[0]
+	args = parser.parse_args()
 
-	if options.strong:
-		options.charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*+-'
+	if args.strong:
+		args.charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*+-'
 
-	for x in range(0, options.count):
-		print ''.join( [options.charset[ randint(0, len(options.charset)-1) ] for x in range(0, options.length)] )
+	for x in range(0, args.count):
+		print ''.join( [args.charset[ randint(0, len(args.charset)-1) ] for x in range(0, args.length)] )
