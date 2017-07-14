8d0d580ea4aa665369d0a10020690162405acb71
Latchezar Tzvetkoff <latchezar@tzvetkoff.net>
2017-07-14 12:47:38 +0300

    vim update colors

--- .vim/bundle/update.sh
+++ .vim/bundle/update.sh
@@ -4,26 +4,43 @@
 # This script's path
 #
 
-ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+root="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+#
+# Some colors
+#
+
+reset="\033[0m"
+red="\033[1;31m"
+green="\033[1;32m"
+yellow="\033[1;33m"
 
 #
 # Walk modules.txt and install/update each module
 #
 
 while read -r remote local branch; do
-  if [[ -d "${ROOT}/${local}" && -d "${ROOT}/${local}/.git" ]]; then
-    pushd "${ROOT}/${local}"
+  if [[ -d "${root}/${local}" && -d "${root}/${local}/.git" ]]; then
+    pushd "${root}/${local}" >/dev/null
+    echo -e "-> Updating ${green}${local}${reset} from ${red}${remote}${reset}${branch:+ (${yellow}${branch}${reset})}"
+
     if [[ -n "${branch}" ]]; then
       git checkout "${branch}" && git pull origin "${branch}"
     else
       git pull origin
     fi
-    popd
+
+    popd >/dev/null
+    echo
   else
+    echo -e "-> Fetching ${green}${local}${reset} from ${red}${remote}${reset}${branch:+ (${yellow}${branch}${reset})}"
+
     if [[ -n "${branch}" ]]; then
-      git clone "${remote}" --depth 1 --branch "${branch}" "${ROOT}/${local}"
+      git clone "${remote}" --depth 1 --branch "${branch}" "${root}/${local}"
     else
-      git clone "${remote}" --depth 1 "${ROOT}/${local}"
+      git clone "${remote}" --depth 1 "${root}/${local}"
     fi
+
+    echo
   fi
-done < "${ROOT}/modules.txt"
+done < "${root}/modules.txt"
